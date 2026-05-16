f9fdd83a853df456a14890136d560d73272b9404
Saltanat4 <242242@astanait.edu.kz>
2026-05-16 23:08:34 +0500

    2

--- Assets/Scripts/Interactable/Lever.cs
+++ Assets/Scripts/Interactable/Lever.cs
@@ -1,12 +1,24 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Lever : Interactable
 {
     private bool turnOn = false;
 
+    [SerializeField] private UnityEvent<bool> onLeverSwitched;
+
+    [Header("Animation")]
+    [SerializeField] private AnimationCurve speedCurve;
+    [SerializeField] private Transform stick;
+    [SerializeField] private float animationDuration = 0.5f;
+    private float rotationAngle = 60f;
+    private Coroutine animationCoroutine = null;
+
     void Start()
     {
         PromptMessage = turnOn ? "Turn Off" : "Turn On";
+        animationCoroutine = StartCoroutine(AnimateLever(turnOn));
     }
 
     protected override void Interact(GameObject interacter)
@@ -14,9 +26,34 @@ public class Lever : Interactable
         base.Interact(interacter);
 
         turnOn = !turnOn;
-        
-        Debug.Log($"Lever {turnOn}");
-
+        onLeverSwitched.Invoke(turnOn);
         PromptMessage = turnOn ? "Turn Off" : "Turn On";
+
+        if (animationCoroutine != null) StopCoroutine(animationCoroutine);
+        animationCoroutine = StartCoroutine(AnimateLever(turnOn));
+    }
+
+    private IEnumerator AnimateLever(bool isOn)
+    {
+        float elapsed = 0f;
+
+        Quaternion startRotation = stick.localRotation;
+        Quaternion targetRotation = Quaternion.Euler(isOn ? rotationAngle : -rotationAngle, 0, 0);
+
+        while (elapsed < animationDuration)
+        {
+            elapsed += Time.deltaTime;
+
+            float t = elapsed / animationDuration;
+
+            float curveValue = speedCurve.Evaluate(t);
+
+            stick.localRotation = Quaternion.Slerp(startRotation, targetRotation, curveValue);
+
+            yield return null;
+        }
+
+        stick.localRotation = targetRotation;
+        animationCoroutine = null;
     }
 }
---
+++ Assets/Scripts/Props/TwoStatePlatform.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class TwoStatePlatform : MonoBehaviour
+{
+    [SerializeField] private Transform firstPos;
+    [SerializeField] private Transform secondPos;
+    [SerializeField] private Transform platform;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Coroutine moveCoroutine = null;
+
+    public void SetMove(bool toSecond = false)
+    {
+        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
+
+        moveCoroutine = StartCoroutine(Move(toSecond ? secondPos : firstPos));
+    }
+
+    private IEnumerator Move(Transform targetPos)
+    {
+        while (Vector3.Distance(firstPos.position, secondPos.position) > .1f)
+        {
+            platform.position = Vector3.MoveTowards(platform.position, targetPos.position, moveSpeed);
+            yield return null;
+        }
+        yield return null;
+    }
+}
